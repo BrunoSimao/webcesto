769f826f73752238cc90623237c08b48aa828e0e
BrunoSimao <brunosimao550@gmail.com>
2021-05-18 22:49:43 -0300

    teste

--- src/app/pages/cardapio/cardapio.component.ts
+++ src/app/pages/cardapio/cardapio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cardapio',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardapioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  EditarItem() {
+    this.router.navigate(['/detalhe-cardapio']);
   }
 
 }
